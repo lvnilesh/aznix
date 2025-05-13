22674d43400007a99823e9a41fc3158714bc2ccc
Nilesh <nilesh@cloudgeni.us>
2025-05-13 22:28:22 +0000

    rebuild script

--- rebuild.sh
+++ rebuild.sh
@@ -5,7 +5,7 @@ echo "Updating hardware configuration..."
 sudo nixos-generate-config --show-hardware-config > ~/aznix/hardware-configuration.nix
 echo "Hardware configuration updated."
 
-echo "Forcibly installing systemd-boot..."
+echo "Forcibly installing systemd-boot...the operation to find bootctl in nix store takes some time"
 sudo "$(find /nix/store -name bootctl | head -1)" install --path=/boot
 
 echo "Rebuilding NixOS..."
